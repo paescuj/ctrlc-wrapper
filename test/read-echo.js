29bc9a2cbf532fe17b150d76b9d54edb7ecda05f
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-08-18 13:05:10 +0200

    Run prettier & fix package.json

--- test/read-echo.js
+++ test/read-echo.js
@@ -1,15 +1,15 @@
-process.on("SIGINT", () => {
-  console.log("SIGINT");
+process.on('SIGINT', () => {
+  console.log('SIGINT');
   process.exit(0);
 });
 
-process.stdin.on("data", (chunk) => {
+process.stdin.on('data', (chunk) => {
   const line = chunk.toString().trim();
-  if (line === "stop") {
+  if (line === 'stop') {
     process.exit(0);
   }
 
   console.log(line);
 });
 
-console.log("READING");
+console.log('READING');
