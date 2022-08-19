82bc34ce319dc54bc76ececda08c680b07943191
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-08-19 14:08:15 +0200

    Fix types

--- index.ts
+++ index.ts
@@ -4,19 +4,40 @@ import {
   ChildProcessWithoutNullStreams,
 } from 'child_process';
 
-interface Wrapper extends ChildProcessWithoutNullStreams {
+interface WrapperChildProcessWithoutNullStreams
+  extends ChildProcessWithoutNullStreams {
   sendCtrlC(): void;
 }
 
 function spawnWithWrapper(
   command: string,
+  options?: SpawnOptionsWithoutStdio
+): WrapperChildProcessWithoutNullStreams;
+function spawnWithWrapper(
+  command: string,
   args?: readonly string[],
   options?: SpawnOptionsWithoutStdio
-): Wrapper {
+): WrapperChildProcessWithoutNullStreams;
+
+function spawnWithWrapper(
+  command: string,
+  argsOrOptions?: readonly string[] | SpawnOptionsWithoutStdio,
+  options?: SpawnOptionsWithoutStdio
+): WrapperChildProcessWithoutNullStreams {
+  if (
+    typeof argsOrOptions === 'object' &&
+    !Array.isArray(argsOrOptions) &&
+    argsOrOptions !== null
+  ) {
+    return spawnWithWrapper(
+      command,
+      [],
+      argsOrOptions as SpawnOptionsWithoutStdio
+    );
+  }
+  const args = (argsOrOptions || []) as string[];
   if (process.platform === 'win32') {
-    if (args) {
-      (args as string[]).unshift(command);
-    }
+    args.unshift(command);
     const arch: Partial<Record<NodeJS.Architecture, string>> = {
       x64: '64',
       ia32: '32',
@@ -25,7 +46,11 @@ function spawnWithWrapper(
       `ctrlc-wrapper-windows-${arch[process.arch]}/start.exe`
     );
   }
-  const child = spawn(command, args, options) as Wrapper;
+  const child = spawn(
+    command,
+    args,
+    options
+  ) as WrapperChildProcessWithoutNullStreams;
   child.sendCtrlC = () => {
     if (process.platform === 'win32') {
       child.stdin.write('^C\n');
